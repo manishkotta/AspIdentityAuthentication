d744b0439e77742ffb9472b78b526b3c23b770d3
Manish Kotta <manish.kotta@ggktech.local>
2018-01-05 19:05:11 +0530

    Integrated twilio API to MVC Project

---
+++ AspIdentityAuthentication/Keys.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspIdentityAuthentication
+{
+    public static class Keys
+    {
+        public static string SMSAccountIdentification = "AC7aaeb51c9e4e60972bf49cd5ef852095";
+        public static string SMSAccountPassword = "382ecdcf108ff2bbc8b6f66894be1eb9";
+        public static string SMSAccountFrom = "+17275652788";
+    }
+}
---
+++ AspIdentityAuthentication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspIdentityAuthentication.Startup))]
+namespace AspIdentityAuthentication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
